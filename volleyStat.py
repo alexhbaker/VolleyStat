b8658eac199c894678a33027eb0567e0e030d93b
Alex Baker <alexhbaker@gmail.com>
2024-09-08 23:40:30 -0400

    Adding meters for player kill percentage and efficiency

--- volleyStat.py
+++ volleyStat.py
@@ -17,24 +17,25 @@ def newPlayerButton():
     FrameObject.frameObject(output.get("1.0","end-1c"), numberOutput.get("1.0","end-1c"), frame, row)
     row+=1
 
-window = ttk.Window(themename="darkly")
+window = ttk.Window(themename="darkly", size=[1000,400])
 window.title("VolleyStat - Fun, Free statistics")
 
 frame = ttk.Frame(window)
 frame.pack()
 
-initialFrame = ttk.LabelFrame(frame, text="User Info")
+initialFrame = ttk.LabelFrame(frame, text="User Info", bootstyle="info")
+initialFrame.columnconfigure((0,1), weight=1, pad=5)
 initialFrame.grid(row = 1, column = 0)
 
-initialFrameButton = ttk.Button(initialFrame, text="New Player", command = newPlayerButton)
-initialFrameButton.grid(row=0, column=1)
+initialFrameButton = ttk.Button(initialFrame, text="New Player", command = newPlayerButton, bootstyle="success")
+initialFrameButton.grid(row=0, column=1, padx=5, pady=2)
 nameText = ttk.Label(initialFrame, text="Name: ")
 nameText.grid(row=0, column=2)
 output = ttk.Text(initialFrame, height = 1, width = 10, bg = "light cyan")
-output.grid(row=0, column=3)
+output.grid(row=0, column=3, padx=5, pady=2)
 numberText = ttk.Label(initialFrame, text="Number: ")
 numberText.grid(row=0, column=4)
 numberOutput = ttk.Text(initialFrame, height=1, width=2, bg="light cyan")
-numberOutput.grid(row=0,column=5)
+numberOutput.grid(row=0,column=5, padx=5, pady=2)
 
 window.mainloop()
