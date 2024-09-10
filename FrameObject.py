2e46eef5f38ec7d0686d975ac3c2953bea764289
Alex Baker <alexhbaker@gmail.com>
2024-09-09 23:15:14 -0400

    Fixed the efficiency bar color change and resized kill % meter

--- FrameObject.py
+++ FrameObject.py
@@ -9,45 +9,70 @@ def frameObject(name: str, number: int, frame, rowNum: int) -> ttk.LabelFrame:
     
     def hitInPlayButton():
         player.hitInPlay()
-        hitEfficiency = f"{((player.he*50)+50):.4f}"
-        initialFrameLabel.config(text= "Efficiency: " + str(hitEfficiency))
+        progressBarColor()
+        hitEfficiency = hittingEfficiencyString()
+        initialFrameLabel.config(text= "Efficiency: " + hitEfficiency)
         progressFrame['value'] = hitEfficiency
         meterFrame.amountusedvar.set(player.kp)
 
     def killButton():
         player.killScored()
-        hitEfficiency = f"{((player.he*50)+50):.4f}"
-        initialFrameLabel.config(text= "Efficiency: " + str(hitEfficiency))
+        progressBarColor()
+        hitEfficiency = hittingEfficiencyString()
+        initialFrameLabel.config(text= "Efficiency: " + hitEfficiency)
         progressFrame['value'] = hitEfficiency
         meterFrame.amountusedvar.set(player.kp)
 
     def errorButton():
         player.errorRecorded()
-        if player.he < 0:
-            meterFrame['bootstyle'] = 'Danger'
-        hitEfficiency = f"{((player.he*50)+50):.4f}"
-        initialFrameLabel.config(text= "Efficiency: " + str(hitEfficiency))
+        progressBarColor()
+        hitEfficiency = hittingEfficiencyString()
+        initialFrameLabel.config(text= "Efficiency: " + hitEfficiency)
+        print(len(str(player.he)))
         progressFrame['value'] = hitEfficiency
         meterFrame.amountusedvar.set(player.kp)
 
+    def hittingEfficiencyString() -> str:
+        hitEfficiency = f"{((player.he*50)+50):.4f}"
+        hitEfficiency.ljust(6, '0')
+        return hitEfficiency
+
+    def progressBarColor():
+        s = ttk.Style()
+        s.configure('red.Vertical.TProgressbar',background="#e74c3c", lightcolor="#e74c3c", darkcolor="#e74c3c")
+        s.configure('yellow.Vertical.TProgressbar',background="#f39c12", lightcolor="#f39c12", darkcolor="#f39c12")
+        s.configure('green.Vertical.TProgressbar',background="#00bc8c", lightcolor="#00bc8c", darkcolor="#00bc8c")
+        if player.he < 0:
+            # progressFrame['style'] = 'Danger'
+            
+            progressFrame['style'] = "red.Vertical.TProgressbar"
+        elif player.he > .3:
+            progressFrame['style'] = 'green.Vertical.TProgressbar'
+        else:
+            progressFrame['style'] = 'yellow.Vertical.TProgressbar'
+
+    # Creating the player object
     player = Player.Player(name, number)
-    userText = name + " #" + str(number) 
+
+    # Creating the frame that the player's stats and tracking will be in 
+    userText = name + " #" + str(number)
     initialFrame = ttk.LabelFrame(frame, text=userText)
     initialFrame.columnconfigure((0,1), weight=1)
     initialFrame.grid(row = rowNum, column = 0)
 
     initialFrameLabel = ttk.Label(initialFrame, text="Efficiency: " + str(player.he))
-    initialFrameLabel.grid(row=0, column=0, padx=5, pady=2)
-    initialFrameButton = ttk.Button(initialFrame, text="Hit in play", command=hitInPlayButton, bootstyle="outline-info")
-    initialFrameButton.grid(row=0, column=1, padx=5, pady=2)
-    initialFrameButton1 = ttk.Button(initialFrame, text="Kill", command=killButton, bootstyle="outline-success")
-    initialFrameButton1.grid(row=0, column=2, padx=5, pady=2)
-    initialFrameButton2 = ttk.Button(initialFrame, text="Error", command=errorButton, bootstyle="outline-danger")
-    initialFrameButton2.grid(row=0, column=3, padx=5, pady=2)
 
-    progressFrame = ttk.Progressbar(initialFrame, value=50, orient='vertical', style='Warning', )
+    initialFrameLabel.grid(row=0, column=3, padx=5, pady=2)
+    initialFrameButton = ttk.Button(initialFrame, text="Hit in play", command=hitInPlayButton, bootstyle="outline-info")
+    initialFrameButton.grid(row=0, column=0, padx=5, pady=2)
+    initialFrameButton1 = ttk.Button(initialFrame, text="Kill", command=killButton, bootstyle="outline-success")
+    initialFrameButton1.grid(row=0, column=1, padx=5, pady=2)
+    initialFrameButton2 = ttk.Button(initialFrame, text="Error", command=errorButton, bootstyle="outline-danger")
+    initialFrameButton2.grid(row=0, column=2, padx=5, pady=2)
+
+    progressFrame = ttk.Progressbar(initialFrame, value=50, style='Danger.Vertical', orient='vertical')
     progressFrame.grid(row=0, column=4, padx=5, pady=2)
 
-    meterFrame = ttk.Meter(initialFrame, amountused=player.kp, arcrange=180, interactive=False, amounttotal=100, subtext="Kill Percentage", textright="%")
+    meterFrame = ttk.Meter(initialFrame, amountused=player.kp, arcrange=180, interactive=False, amounttotal=100, subtext="Kill Percentage", textright="%", metersize=150)
     meterFrame.grid(row=0, column=5, padx=5, pady=2)
     pass
